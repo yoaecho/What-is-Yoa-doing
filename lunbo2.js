e9b6a22c9ce0dde0f3eaae0ce08f5b13e93268fd
yoaecho <130732343+yoaecho@users.noreply.github.com>
2023-06-14 19:48:30 +0800

    6.14

--- lunbo2.js
+++ lunbo2.js
@@ -7,17 +7,11 @@ const itemWidth = sliderItems[0].offsetWidth;
 
 let current = 0;
 
-prevBtn.addEventListener('click', () => {
-  current = current > 0 ? current - 1 : sliderItems.length - 1;
-  sliderContainer.style.transform = `translateX(-${current * itemWidth}px)`;
-});
+prevBtn.style.display = 'none'; // 隐藏左滑动箭头
+nextBtn.style.display = 'none'; // 隐藏右滑动箭头
 
-nextBtn.addEventListener('click', () => {
+// 创建一个定时器，每2秒钟自动切换到下一张图片
+setInterval(() => {
   current = current < sliderItems.length - 1 ? current + 1 : 0;
   sliderContainer.style.transform = `translateX(-${current * itemWidth}px)`;
-});
-
-// 创建一个定时器，每2秒钟自动点击 nextBtn
-setInterval(() => {
-  nextBtn.click();
 }, 2000);
